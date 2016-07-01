e76bdba5c32dc09d4a75c02e2da676782da1c95b
sixwhale <wjsixwhale@gmail.com>
2016-07-01 20:29:14 +0800

    ThinkPHP

---
+++ Application/Home/Controller/LoginController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+class LoginController extends Controller {
+
+  //登陆
+  public function login() {
+    $this->display();
+  }
+
+  //注册
+  public function register() {
+    $this->display();
+  }
+}
+ ?>
